62083e9626eaa8eee246cae62cb006cc3b3607d9
EternalBlueFlame <jakedude435@gmail.com>
2025-07-06 14:50:59 -0500

    Render fixes, new bogie render added
    - Fixed a lag issue with the new API that caused models not to cache, and instead create multiple new instances every frame.
    - Bogie models from the new API should now render.

--- src/main/java/train/client/render/TransportRenderCache.java
+++ src/main/java/train/client/render/TransportRenderCache.java
@@ -1,7 +1,12 @@
 package train.client.render;
 
+import fexcraft.tmt.slim.ModelBase;
+
 import java.util.ArrayList;
 
 public class TransportRenderCache {
-    ArrayList<double[]> smokePosition=null;
+    public ArrayList<double[]> smokePosition = null;
+    public ModelBase[] models = null;
+    public Bogie[] bogies = null;
+    public boolean needs_model_update = true;
 }
